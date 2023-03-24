b4b3abf8790d563fbc6dc38df3aee0a9cfe8f981
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-03-24 09:04:19 +0300

    app

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Signup from './Signup';
+import Login from './Login';
+import Home from './Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
