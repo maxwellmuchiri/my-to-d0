8e000adb955119397f247b027e3baf9916ac3966
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-03-24 09:11:43 +0300

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,25 @@
+import React from 'react';
+
+function Home() {
+  const todos = [
+    { id: 1, title: 'Todo 1' },
+    { id: 2, title: 'Todo 2' },
+    { id: 3, title: 'Todo 3' }
+  ];
+
+  return (
+    <div>
+      <h1>My To-Dos</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+      <button>Update To-Dos</button>
+      <button>Delete To-Dos</button>
+    </div>
+  );
+}
+
+export default Home;
 
