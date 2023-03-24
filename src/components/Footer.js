d8f2d60fb7feb4a20e6c25bb6b35a3d93f8d7384
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-03-24 08:54:33 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+function Footer() {
+  return (
+    <footer className="bg-light text-center text-lg-start">
+      <div className="text-center p-3">
+        © {new Date().getFullYear()} My To-Dos
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
